03eab0975150258cf2fb8116b5a8dc412b6600af
pjs020304 <pjs020304@naver.com>
2025-11-15 21:31:56 +0900

    GameSessionLoop 구현
    플레이어 초기 위치나 공 위치, 키퍼 위치는 수정 부탁드립니다.
    추가로 사이즈에 header 사이즈까지 포함된 크기를 전체를 보내기 때문에 만약 수정시 아래 send도 수정해야 합니다.

--- 방과 후 축구 한판 Server/방과 후 축구 한판 Server/main.cpp
+++ 방과 후 축구 한판 Server/방과 후 축구 한판 Server/main.cpp
@@ -10,6 +10,110 @@
 PacketInputkey g_LatestInputKey[MAX_PLAYERS];
 PacketInputspecialkey g_LatestInputSpecialKey[MAX_PLAYERS];
 
+// --- 전역 변수 보호용 Lock ---
+CRITICAL_SECTION g_InputLock;
+
+void GameSessionLoop(SOCKET clientSockets[]) {
+
+    // --- 게임 월드(World) 초기화 (수정 필요)---
+    Player players[MAX_PLAYERS];
+    Ball ball(0, 0, 0);
+    Keeper keeper(0, 0, -32);
+
+    // 플레이어 초기 위치 설정 - (수정 필요)
+    players[0].setPosition(0, 0, 5);
+    players[1].setPosition(3, 0, 5);
+    players[2].setPosition(-3, 0, 5);
+
+    time_t startTime = time(NULL);
+
+    //PacketRenderData 준비
+    PacketRenderData statePkt;
+
+    std::cout << "--- Game Loop Started ---" << std::endl;
+
+    // --- 메인 루프 시작 ---
+    while (true) {
+
+
+        // Lock을 걸고 연산 시작 
+        EnterCriticalSection(&g_InputLock);
+
+        // 저장된 최신 데이터를 지역 변수로 복사 
+        PacketInputkey playerKeys[MAX_PLAYERS];
+        PacketInputspecialkey playerSpecialKeys[MAX_PLAYERS];
+
+        memcpy(playerKeys, g_LatestInputKey, sizeof(g_LatestInputKey));
+        memcpy(playerSpecialKeys, g_LatestInputSpecialKey, sizeof(g_LatestInputSpecialKey));
+
+        // 다음 틱을 위해 전역 변수 초기화 
+        memset(g_LatestInputKey, 0, sizeof(g_LatestInputKey));
+        memset(g_LatestInputSpecialKey, 0, sizeof(g_LatestInputSpecialKey));
+
+        // Lock 해제
+        LeaveCriticalSection(&g_InputLock);
+
+
+        // 저장된 최신 데이터로 연산 (게임 로직)
+
+        // 입력값 적용
+        for (int i = 0; i < MAX_PLAYERS; i++) {
+            for (int k = 0; k < 256; k++) {
+                if (playerKeys[i].key[k]) players[i].keyDown(k);
+                else players[i].keyUp(k);
+
+                if (playerSpecialKeys[i].specialkey[k]) players[i].keyDown(k);
+                else players[i].keyUp(k);
+            }
+        }
+
+        // 객체 Move() 호출
+        bool anyPlayerHasBall = false;
+        for (int i = 0; i < MAX_PLAYERS; i++) {
+            players[i].Move(ball, keeper.ishasBall());
+            if (players[i].ishasBall()) anyPlayerHasBall = true;
+        }
+        keeper.Move(ball.getPosition(), anyPlayerHasBall);
+        ball.Move(keeper.getPosition(), keeper.ishasBall());
+
+        // 충돌 및 골, 시간 종료 등
+        for (int i = 0; i < MAX_PLAYERS; i++) {
+            Gameover(time(NULL) - startTime, clientSockets[i]);
+        }
+
+
+        // 데이터 보내기
+
+        // 3명의 플레이어 데이터 채우기
+        for (int i = 0; i < MAX_PLAYERS; i++) {
+            statePkt.p_data[i].id = i; // 플레이어 ID
+            statePkt.p_data[i].position = players[i].getPosition();
+            statePkt.p_data[i].rotation = players[i].getRotation();
+        }
+
+        // 공, 키퍼 데이터 채우기
+        statePkt.b_data.position = ball.getPosition();
+        statePkt.b_data.rotation = ball.getRotation();
+
+        statePkt.k_data.position = keeper.getPosition();
+        statePkt.k_data.rotation = keeper.getRotation();
+
+        // 헤더 갱신 (사이즈 일단은 전부로 했는데, 만약 필요시 PacketRenderData - header로 수정하면 아래 send도 한번에 보내기 때문에 수정해야 합니다.)
+        statePkt.header.type = htons(PKT_RENDER_DATA);
+        statePkt.header.size = htons(sizeof(PacketRenderData));
+
+        // 모든 클라이언트에게 'send' 
+        for (int i = 0; i < MAX_PLAYERS; i++) {
+            if (send(clientSockets[i], (char*)&statePkt, sizeof(statePkt), 0) == SOCKET_ERROR) {
+                std::cerr << "Send failed to player " << i << std::endl;
+            }
+        }
+
+        // 30 FPS
+        Sleep(33);
+    }
+}
+
 int main() {
 
     // --- 리슨 소켓 설정 ---
